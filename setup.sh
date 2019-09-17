e4f61ac2775d00800446a7627a1cf3918903ed85
okaymunsi <munsefar.khaleque@gmail.com>
2019-09-16 22:24:36 -0500

    Start Setup Script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Start of setup script."
+
+git clone git://github.com/okaymunsi/wornwedge.git
+
+# Enter workspace
+cd wornwedge
+
+# Setup the VENV for the project
+source env/bin/activate
+
+# Check Packages
+# 1. Ruby
+# 2. Gems
+# 2. Bundler
+# 3. Jekyll
+https://jekyllrb.com/docs/step-by-step/01-setup/
+# 4. Apache2
+https://vitux.com/how-to-install-and-configure-apache-web-server-on-ubuntu/
+# 5. https/SSL
+https://www.namecheap.com/support/knowledgebase/article.aspx/804/69/ssl-certificate-activation-and-installation-for-domains-hosted-on-namecheap-hosting-servers
+3158
