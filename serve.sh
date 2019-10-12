545ebdc3abe8b3f60cc917367cbd43130102e4ff
okaymunsi <munsefar.khaleque@gmail.com>
2019-10-12 00:09:35 -0500

    Build, sync, setup /var/www/, setup /etc/nginx/sites-XXX

--- serve.sh
+++ serve.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-set -e
 
 # Argument checking
 if [ $# -ne 1 ]
   then
     echo "No arguments supplied"
+    exit 1
 fi
 
-site='wornwedge'
+SITE="wornwedge"
 # Check if in correct dir
-[[ -d "$PWD" =~ $1 ]] || { echo "Please be in $1/ dir"; exit 1; }
+[[ "$PWD" =~ $1 ]] || { echo "Please be in $1/ dir"; exit 1; }
 
 # Build site
 bundle exec jekyll build
-[[ -d $PWD/$1/_site ]] || { echo "Could not find $PWD/$1/_site"; exit 1; }
+[[ -d $PWD/_site ]] || { echo "Could not find $PWD/_site"; exit 1; }
 
 # Check if /var/www/ is setup
-[[ -d /var/www/$site/$1 ]] || { echo "Making /var/www/$site/$1"; mkdir /var/www/$site/$1; }
-rsync -avh --dry-run $PWD/$1/_site /var/www/$site/$1
+[[ -d /var/www/$SITE/$1 ]] || { echo "Making /var/www/$SITE/$1"; mkdir -p /var/www/$SITE/$1; }
+rsync -avh $PWD/_site/* /var/www/$SITE/$1 || { echo "rsync failed"; exit 1; }
 
 # Setup /nginx sites
-[[ -f /etc/nginx/sites-available/$site.com ]] || { echo "server {\nlisten 80;\nroot /var/www/$site/$1;\nindex index.html;\nserver_name www.$site.com;\n}" > /etc/nginx/sites-available/$site.com && echo "Created /etc/nginx/sites-available/$site.com" }
+[[ -f /etc/nginx/sites-available/$SITE.com ]] || { printf "server {\nlisten 80;\nroot /var/www/$SITE/$1;\nindex index.html;\nserver_name www.$SITE.com;\n}\n" > /etc/nginx/sites-available/$SITE.com && echo "Created /etc/nginx/sites-available/$SITE.com"; }
 
-[[ -f /etc/nginx/sites-enabled/$site.com ]] || { ln -s /etc/nginx/sites-available/$site.com /etc/nginx/sites-enabled/ }
+[[ -f /etc/nginx/sites-enabled/$SITE.com ]] || { ln -s /etc/nginx/sites-available/$SITE.com /etc/nginx/sites-enabled/; }
+
+exit 0
